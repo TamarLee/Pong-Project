a83f4dddb5758cb404dffcb25d9ff50d246a7326
DESKTOP-IVAB5BU\user <tmekhri@gmail.com>
2018-04-22 09:39:46 +0400

    ფონი და სათამაშო ოთკუთხედი

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class MainClass extends JComponent {
+
     public static void main(String[] args){
         JFrame window = new JFrame("Pong Game by T.Mekhri");
         window.add(new MainClass());
@@ -8,5 +10,22 @@ public class MainClass extends JComponent {
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(800, 600);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        g.setColor(new Color(191, 245, 245));
+        g.fillRect(0, 0, 800, 600);
+
+        g.setColor(new Color(45, 111, 45));
+        g.fillRect(100, 500, 80, 20);
+
+
     }
 }
